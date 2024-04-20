f0ba62a797c08cb67415a5f82ff689b1ceb70acf
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-04-19 23:27:08 -0300

    Personagens Favoritos

--- Meus Projetos/Cartão/script.js
+++ Meus Projetos/Cartão/script.js
@@ -7,7 +7,8 @@ function virar() {
         Se a classe especificada ('virar') estiver presente no elemento, ela será removida.
         Se não estiver presente, ela será adicionada.
         Quando clica no botão "Verso", a função virar() é chamada. 
-        Essa função seleciona a div .conteudo e usa classList.toggle('virar') para alternar a presença da classe virar.
+        Essa função seleciona a div .conteudo e usa classList.toggle('virar') para alternar a presença da 
+        classe virar.
         
         Isso significa que se a classe .virar não estiver presente,
         ela será adicionada (o cartão irá girar para mostrar o verso).*/
---
+++ Meus Projetos/Orochimaru/script.js
@@ -0,0 +1,9 @@
+function virar(){
+    let conteudo = document.querySelector(".conteudo");
+    conteudo.classList.toggle("virar");
+}
+
+function desvirar(){
+    let conteudo = document.querySelector(".conteudo");
+    conteudo.classList.remove("virar");
+}
