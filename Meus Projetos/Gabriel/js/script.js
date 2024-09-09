17d96da4c2f9526650eae090b959be83985c9f39
GabrielXavierD <gabrielxavierdominguess@gmail.com>
2024-09-08 21:34:02 -0300

    .

---
+++ Meus Projetos/Gabriel/js/script.js
@@ -0,0 +1,3 @@
+function redirecionaProduto(id){
+id == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 ?  window.location.href = "../lojaProdutos/camiseta" + id + ".html" : alert("ID de botão inválido");
+}
